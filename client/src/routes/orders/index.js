ba6829716472a72da4260588c64a31383eb69423
grom194 <brskoshelev@gmail.com>
2019-09-26 23:49:55 +0300

    add total cost output

--- client/src/routes/orders/index.js
+++ client/src/routes/orders/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Card, Heading } from '@8base/boost';
+import { Card, Heading, Paper } from '@8base/boost';
+import { graphql, Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import { OrderCreateDialog } from './OrderCreateDialog';
 import { OrderEditDialog } from './OrderEditDialog';
 import { OrderDeleteDialog } from './OrderDeleteDialog';
 import { OrdersTable } from './OrdersTable';
 
+const getTotalCostOfOrders = gql`
+query {
+  getTotalCostOfOrders {
+    totalCost
+  }
+}
+`;
+
 const Orders = () => (
   <Card padding="md" stretch>
     <Card.Header>
-      <Heading type="h4" text=" Orders" />
+      <Heading type="h4" text=" Orders" />  
     </Card.Header>
 
     <OrderCreateDialog />
@@ -18,6 +28,14 @@ const Orders = () => (
     <Card.Body padding="none" stretch scrollable>
       <OrdersTable />
     </Card.Body>
+    <Card.Footer>
+    <Query query={getTotalCostOfOrders}>
+      {({loading, data}) => {
+        if (loading) return 'Total Cost';
+      return `Total Cost: ${data.getTotalCostOfOrders.totalCost}`
+      }}
+      </Query>
+    </Card.Footer>
   </Card>
 );
 
